39bdb07828c551cc2926d8cda063164c67de95f4
Jeremy Andrews <jeremy@tag1consulting.com>
2026-05-06 06:30:00 +0200

    Fix wp-config-container.php: guard defines to prevent WP double-load (#10)
    * Remove upstreamed demo patches and sync scolta.js to canonical
    
    Both demo patches (URL portability + filters, HTML entity stripping)
    are now in scolta-php main. Removed patch files and application hooks.
    
    * Upgrade Scolta to 0.3.10
    
    * Fix wp-config-container.php: guard defines to prevent double-load of WP core

--- wp-config-container.php
+++ wp-config-container.php
@@ -1,38 +1,33 @@
 <?php
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+defined( 'DB_CHARSET' ) || define( 'DB_CHARSET', 'utf8' );
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+defined( 'DB_COLLATE' ) || define( 'DB_COLLATE', '' );
 
 /** Authentication Unique Keys and Salts. */
-define( 'AUTH_KEY', getenv( 'WP_AUTH_KEY' ) );
-define( 'SECURE_AUTH_KEY', getenv( 'WP_SECURE_AUTH_KEY' ) );
-define( 'LOGGED_IN_KEY', getenv( 'WP_LOGGED_IN_KEY' ) );
-define( 'NONCE_KEY', getenv( 'WP_NONCE_KEY' ) );
-define( 'AUTH_SALT', getenv( 'WP_AUTH_SALT' ) );
-define( 'SECURE_AUTH_SALT', getenv( 'WP_SECURE_AUTH_SALT' ) );
-define( 'LOGGED_IN_SALT', getenv( 'WP_LOGGED_IN_SALT' ) );
-define( 'NONCE_SALT', getenv( 'WP_NONCE_SALT' ) );
+defined( 'AUTH_KEY' ) || define( 'AUTH_KEY', getenv( 'WP_AUTH_KEY' ) );
+defined( 'SECURE_AUTH_KEY' ) || define( 'SECURE_AUTH_KEY', getenv( 'WP_SECURE_AUTH_KEY' ) );
+defined( 'LOGGED_IN_KEY' ) || define( 'LOGGED_IN_KEY', getenv( 'WP_LOGGED_IN_KEY' ) );
+defined( 'NONCE_KEY' ) || define( 'NONCE_KEY', getenv( 'WP_NONCE_KEY' ) );
+defined( 'AUTH_SALT' ) || define( 'AUTH_SALT', getenv( 'WP_AUTH_SALT' ) );
+defined( 'SECURE_AUTH_SALT' ) || define( 'SECURE_AUTH_SALT', getenv( 'WP_SECURE_AUTH_SALT' ) );
+defined( 'LOGGED_IN_SALT' ) || define( 'LOGGED_IN_SALT', getenv( 'WP_LOGGED_IN_SALT' ) );
+defined( 'NONCE_SALT' ) || define( 'NONCE_SALT', getenv( 'WP_NONCE_SALT' ) );
 
 /** Absolute path to the WordPress directory. */
 defined( 'ABSPATH' ) || define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 
-define( 'DB_NAME', getenv( 'DB_NAME' ) );
-define( 'DB_USER', getenv( 'DB_USERNAME' ) );
-define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
-define( 'DB_HOST', getenv( 'DB_HOST' ) );
-define( 'WP_HOME', getenv( 'WP_HOME' ) );
-define( 'WP_SITEURL', getenv( 'WP_SITEURL' ) );
-define( 'WP_DEBUG', filter_var( getenv( 'WP_DEBUG' ), FILTER_VALIDATE_BOOLEAN ) );
+defined( 'DB_NAME' ) || define( 'DB_NAME', getenv( 'DB_NAME' ) );
+defined( 'DB_USER' ) || define( 'DB_USER', getenv( 'DB_USERNAME' ) );
+defined( 'DB_PASSWORD' ) || define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
+defined( 'DB_HOST' ) || define( 'DB_HOST', getenv( 'DB_HOST' ) );
+defined( 'WP_HOME' ) || define( 'WP_HOME', getenv( 'WP_HOME' ) );
+defined( 'WP_SITEURL' ) || define( 'WP_SITEURL', getenv( 'WP_SITEURL' ) );
+defined( 'WP_DEBUG' ) || define( 'WP_DEBUG', filter_var( getenv( 'WP_DEBUG' ), FILTER_VALIDATE_BOOLEAN ) );
 
 if ( ! isset( $table_prefix ) || empty( $table_prefix ) ) {
 	// phpcs:disable WordPress.WP.GlobalVariablesOverride.Prohibited
 	$table_prefix = 'wp_';
 	// phpcs:enable
-}
-
-/** Include wp-settings.php */
-if ( file_exists( ABSPATH . '/wp-settings.php' ) ) {
-	require_once ABSPATH . '/wp-settings.php';
 }
